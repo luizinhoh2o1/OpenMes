c2cefc9c09c46b9d826f2e9a73f1d2abd01350fd
Mateusz Łuczyński <mateusz.luczynski01@gmail.com>
2026-04-27 22:17:04 +0200

    feat: openapi

---
+++ backend/config/scramble.php
@@ -0,0 +1,58 @@
+<?php
+
+use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
+
+return [
+    /*
+     * Your API path. By default, all routes starting with this path will be added to the docs.
+     */
+    'api_path' => 'api',
+
+    /*
+     * Your API domain. By default, app domain is used. This is also a part of the default API routes
+     * matcher, so when generating, only routes from this domain will be considered.
+     */
+    'api_domain' => null,
+
+    /*
+     * The path where your OpenAPI specification will be exported.
+     */
+    'export_path' => 'api.json',
+
+    'info' => [
+        'version' => env('API_VERSION', '1.0.0'),
+        'description' => 'OpenMES API documentation. Sign in via Sanctum (POST /api/login) and use the returned bearer token to authorise requests.',
+    ],
+
+    /*
+     * UI tweaks.
+     */
+    'ui' => [
+        'title' => 'OpenMES API',
+        'theme' => 'light',
+        'hide_try_it' => false,
+        'hide_schemas' => false,
+        'logo' => '',
+        'try_it_credentials_policy' => 'include',
+        'layout' => 'responsive',
+    ],
+
+    /*
+     * The list of servers of the API. By default, when `null`, server URL will be created from
+     * `scramble.api_path` and `scramble.api_domain` config variables. When providing an array, you
+     * will need to specify the local server when needed manually.
+     */
+    'servers' => null,
+
+    /**
+     * Determines how Scramble stores the descriptions of enum cases.
+     */
+    'enum_cases_description_strategy' => 'description',
+
+    'middleware' => [
+        'web',
+        RestrictedDocsAccess::class,
+    ],
+
+    'extensions' => [],
+];
