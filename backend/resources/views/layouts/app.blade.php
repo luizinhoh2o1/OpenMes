ecab85ba6578b6b9d39c77e44f846dde2a819dc7
jakub-przepiora <jakub.przepioraa@gmail.com>
2026-05-12 22:37:23 +0200

    feat: add favicon link to all layouts (app, auth, onboarding)

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="OpenMES">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png">
     <link rel="apple-touch-icon" href="/icon-192.png">
     <script>
         /* Apply dark class immediately to avoid flash */
