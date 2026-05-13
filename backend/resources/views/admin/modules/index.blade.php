c2309bb9f023fb5789077d6aa7b672154d096f9e
jakub-przepiora <jakub.przepioraa@gmail.com>
2026-05-13 09:53:11 +0200

    fix: translate Enabled/Disabled badges on modules page

--- backend/resources/views/admin/modules/index.blade.php
+++ backend/resources/views/admin/modules/index.blade.php
@@ -44,11 +44,11 @@
                                 <span class="text-xs text-gray-400 font-mono">v{{ $module['version'] ?? '?' }}</span>
                                 @if($module['enabled'])
                                     <span class="px-2 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-700 dark:bg-blue-900/30 dark:text-blue-300">
-                                        Enabled
+                                        {{ __("Enabled") }}
                                     </span>
                                 @else
                                     <span class="px-2 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-500 dark:bg-gray-700 dark:text-gray-400">
-                                        Disabled
+                                        {{ __("Disabled") }}
                                     </span>
                                 @endif
                                 @if($module['has_error'])
